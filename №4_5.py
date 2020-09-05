2a40e248e2f0acbd742519add7ebd3c2e9ad83b8
Andrey <pranehs@mail.ru>
2020-09-06 00:12:41 +0300

    Домашнее задание № 4.5

---
+++ №4_5.py
@@ -0,0 +1,4 @@
+from functools import reduce
+my_list = [el for el in range(100, 1001, 2)]
+result = reduce(lambda a, b: a*b, my_list)
+print('Ресультат умножения всех четных чисел от 100 до 1000: {}'.format(result))
